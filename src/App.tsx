947284ac01a18d9749bc2e8e20c64afe3c7422a4
Gleison Rocha <glucasmr@gmail.com>
2024-04-01 22:24:17 -0300

    feat: create login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-screen flex items-start bg-white">
+      <div className="relative w-2/3 h-full flex flex-col">
+        <div className="absolute bottom-[15%] left-[5%] flex flex-col">
+          <h1 className="text-2xl text">Plataforma de cursos completa</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates placeat esse nihil commodi illo quaerat id consequatur ad harum rem laudantium minima hic, ducimus voluptate mollitia alias non dolore vel.</p>
+        </div>
+        <img src="../public/Banner.png" className="w-full h-full object-cover" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className="w-1/3 h-full bg-white flex flex-col p-10 justify-between">
+        <h1 className="font-bold">Boas vindas!<FontAwesomeIcon icon={faEnvelope} /></h1>
+        <p>Entre utilizando uma das opções abaixo</p>
+
+
+        <div className="w-full flex flex-col">
+          <h3 className="text-2xl font-semibold mb-4">Login</h3>
+          <FontAwesomeIcon icon="fa-brands fa-facebook" />
+        </div>
+
+        <div className="w-full flex items-center justify-center">
+          <p>Esqueceu sua senha? <span className="text-blue-700">Recuperar senha</span></p>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
